79d4add65f8361f562447c19545de66a8ab40507
Colin Powell <colin.powell@gmail.com>
2015-08-01 22:44:34 -0400

    Moving to a saner requirements and setup file setup

--- Makefile
+++ Makefile
@@ -3,6 +3,16 @@ install:
 	venv/bin/python setup.py install
 	venv/bin/python manage.py syncdb --noinput
 
+
+deps:
+	sudo apt-get install libtiff5-dev libjpeg8-dev zlib1g-dev libfreetype6-dev liblcms2-dev libwebp-dev tcl8.6-dev tk8.6-dev python-tk 
+	npm install bower
+	
+
+deps_mac:
+	brew install libtiff libjpeg webp little-cms2
+
+
 test:
 	rm -rf .tox
 	detox
--- setup.py
+++ setup.py
@@ -5,26 +5,42 @@ import sys
 version = __import__('outline').__version__
 
 install_requires = [
-    'setuptools',
-    'south==0.8.2',
-    'Django==1.6.4',
-    'django-configurations==0.8',
+    'setuptools==18.0.1',
+    'Django==1.8.3',
+    'django_configurations==0.9-sbrandtb',
     'dj-database-url==0.3.0',
     'pylibmc==1.3.0',
-    'boto==2.9.5',
-    'django-storages==1.1.8',
     'Pillow==2.0.0',
     'django-cache-url==0.8.0',
     'werkzeug==0.9.4',
     'gunicorn==0.17.4',
     'easy-thumbnails==1.2',
-    'django-debug-toolbar==1.1',
+    'django-debug-toolbar==1.3.2',
     'django-extensions==1.3.4',
     'django-braces==1.4.0',
-    'django-allauth==0.16.1',
-    'django-floppyforms==1.1.1',
-    'django-zurb-foundation==5.1.1',
-    'django-custom-user==0.4',
+    'django-localflavor==1.1',
+    'django-allauth==0.22.0',
+    'django-floppyforms==1.5.2',
+    'django-custom-user==0.5',
+    'django-nose==1.4.1',
+    'raven==5.2.0',
+    'factory_boy==2.5.1',
+    'boto==2.9.5',
+    'django-storages==1.1.8',
+    'djangorestframework==3.1.0',
+    'django-cors-headers==1.1.0',
+    'markdown==2.6.1',
+    'django-filter==0.9.2',
+    'django-templated-email==0.4.9',
+    'psycopg2==2.5'
+]
+
+# App specific libraries
+install_requires += [
+]
+
+dep_links = [
+    "https://onec-pypicloud.s3.amazonaws.com/6f45/django_configurations/django-configurations-0.9-sbrandtb.tar.gz?Signature=szSuFV7%2F0uBGsNZo53tOLbQx0j8%3D&Expires=1438552169&AWSAccessKeyId=AKIAIYMHQ75FUPRJ7C4Q#egg=django-configurations-0.9-sbrandtb"
 ]
 
 
@@ -51,6 +67,7 @@ setup(
     author_email='colin.powell@gmail.com',
     packages=find_packages(),
     install_requires=install_requires,
+    dependency_links=dep_links,
     include_package_data=True,
     zip_safe=False,
     tests_require=['tox'],
